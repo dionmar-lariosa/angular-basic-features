35ba18725e5a4e1d971a9b1d3b16a8e23f5ec4be
dionmar-lariosa <main.dionmar@gmail.com>
2023-08-05 21:23:55 +0700

    install and run prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-basic-features';
